9fd11f34d526f54adcec250d6a37ac64b1048888
Arnaud <arnaudadjovi274@gmail.com>
2023-09-06 20:23:10 +0000

    working node project ;)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import {readFile} from 'node:fs/promises'
 
-const content = copyFile('demo.txt', {encoding: 'utf8'})
+const content = readFile('demo.txt', {encoding: 'utf8'})
 content.then((e) => {
     console.log(e)
     throw new Error('Hello word !')
