0e007eb5d9bd1294cd33a11f146a6e75aa3ecc71
Arnaud <arnaudadjovi274@gmail.com>
2023-09-06 19:50:44 +0000

    working in node project ;)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import fs from 'node:fs'
 
-const content = fs.readFile('log.txt', {encoding: 'utf8'}, (e, data) => {
+const content = fs.readFile('demo.txt', {encoding: 'utf8'}, (e, data) => {
     console.log(data)
 })
