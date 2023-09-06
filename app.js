ed015102f652a888a88e0e9f0168ca90820f7481
Arnaud <arnaudadjovi274@gmail.com>
2023-09-06 21:59:21 +0000

    working in node project ;)

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ import { readdir } from 'node:fs/promises';
 import { stat } from 'node:fs'
 
 try {
-    const files = await readdir('./');
-    for (const file of files) {
-        stat(file, (e, s) => {
-            if (s.isFile()){
-                let size = (s.size / 100) + 'ko'
-                console.log(`F - ${file} - ${size}`)
-            }else{
-                let size = (s.size / 100) + 'ko'
-                console.log(`D - ${file} - ${size}`)
-            }
-        })
-    }
+    const files = readdir('./');
+    files.then((files) => {
+        for (const file of files) {
+            stat(file, (e, s) => {
+                if (s.isFile()){
+                    let size = (s.size / 100) + 'ko'
+                    console.log(`F - ${file} - ${size}`)
+                }else{
+                    let size = (s.size / 100) + 'ko'
+                    console.log(`D - ${file}`)
+                }
+            })
+        }
+    })
+
 } catch (err) {
     console.error(err);
 }
